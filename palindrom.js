e70b16e7d33e44721a0a5043417ef5dd737fd43b
Hanna Liubarska <gvg8say@gmail.com>
2024-10-25 11:41:06 +0300

    fifth commit

---
+++ palindrom.js
@@ -0,0 +1,19 @@
+
+
+function nextReverseString(str) {
+    return str.split('').reverse().join('');
+}
+function ispolindrom(str) {
+    const newString = nextReverseString(str)
+    return str.toLowerCase() === newString.toLowerCase()
+}
+const nextInputString = "Кабак";
+
+const nextResult = ispolindrom(nextInputString);
+
+console.log(nextResult);
+
+
+
+
+
---
+++ reverse.js
@@ -0,0 +1,9 @@
+const reverseString = str => str.split('').reverse().join('');
+
+
+
+const inputString = "Hello, friends!";
+
+const result = reverseString(inputString);
+
+console.log(result);
